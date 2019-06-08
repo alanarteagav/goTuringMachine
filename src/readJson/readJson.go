6871ec1af88fc88b86199021fadb6ef993aec500
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2019-06-07 19:55:12 -0500

    Input OK.

---
+++ src/readJson/readJson.go
@@ -0,0 +1,31 @@
+package readJson
+
+import (
+    "encoding/json"
+    "io/ioutil"
+    "fmt"
+)
+
+type JsonTuringMachine struct {
+    Estados        []string
+    Entrada        []string
+    Cinta          []string
+    Inicial          string
+    Blanco           string
+    Finales        []string
+    Transiciones [][]string
+}
+
+func Read(fileName string) JsonTuringMachine{
+    file, fileError := ioutil.ReadFile(fileName)
+    if fileError != nil {
+        fmt.Println("Error. Could not read from file.")
+    }
+	turingMachine := JsonTuringMachine{}
+
+	jsonError := json.Unmarshal([]byte(file), &turingMachine)
+    if jsonError != nil {
+        fmt.Println("Error. Could not read from JSON file.")
+    }
+    return turingMachine
+}
